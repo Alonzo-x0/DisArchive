4276e9e4773f5e6d75d1524034aacf6666f58402
Alonzo <arizagapepe@gmail.com>
2021-08-18 00:12:14 -0400

    Added working zip and archiving
    added zipping all photos into a zip to make sharing easier
    first working edition

--- DisArchive.go
+++ DisArchive.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"errors"
 	"fmt"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+
 	//"strconv"
 
 	//"strconv"
@@ -16,6 +18,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+	//"google.golang.org/api/file/v1"
 )
 
 func downloadImage(url, fileName string) error {
@@ -51,36 +54,33 @@ func downloadImage(url, fileName string) error {
 
 //put the last element of the 50 slice into a global variable then loop it in the message create event !start
 var (
-	lastChatID string
-	medium     int
+	saveLocation = "c:\\Users\\Alonzo\\Programming\\DisArchive\\DisArchive\\images\\"
 )
 
-func archive(s *discordgo.Session, lastChatID, channelID string) string {
+func archive(s *discordgo.Session, lastChatID, channelID string) (error, []string) {
 
 	//index the first and last message, make the last message first to keep going 100 back
+	var urlDl []string
 
 	searchRange := 100
 	message, err := s.ChannelMessages(channelID, searchRange, lastChatID, "", "")
 	if err != nil {
-		log.Println(err)
-		return ""
+
+		return err, urlDl
 	}
 
 	//c:\Users\Alonzo\Programming\DisArchive\DisArchive\images
 	//var messageID []int
 	//gets last element in messages unique ID and makes it global
-	log.Println("LAST CHAT ID: " + lastChatID)
 	foo := len(message) - 1
 	if foo < 0 {
-		return "Out of Range"
+		err := errors.New("Out of range, last chatID: " + lastChatID)
+		//log.Println()
+		return err, urlDl
 	}
 	lastChatID = message[foo].ID
-
 	//look for last message in range, and go another 100 back
-
 	for _, content := range message {
-		medium++
-		log.Println(medium)
 
 		if len(content.Attachments) != 0 {
 
@@ -88,15 +88,18 @@ func archive(s *discordgo.Session, lastChatID, channelID string) string {
 				if foo.Size >= 256 {
 					fileType := strings.SplitAfter(foo.Filename, ".")
 					fileName := foo.ID + "." + fileType[1]
-					log.Println(fileName + foo.URL)
 					//create your own folder for images and place the path below
 					//only creates if file does not exist, file use unique IDs names so it should not make duplicates
-					if _, err := os.Stat("c:\\Users\\Alonzo\\Programming\\DisArchive\\DisArchive\\images\\" + fileName); os.IsNotExist(err) {
+					if _, err := os.Stat(saveLocation + fileName); os.IsNotExist(err) {
 
-						log.Println("Creating file " + fileName)
+						log.Println("Creating file " + fileName + foo.URL)
+
 						err := downloadImage(foo.URL, fileName)
 						if err != nil {
-							log.Println(err)
+							urlDL := append(urlDl, foo.URL)
+							log.Println(urlDL)
+							return err, urlDl
+
 						}
 					}
 
@@ -107,21 +110,41 @@ func archive(s *discordgo.Session, lastChatID, channelID string) string {
 		}
 
 	}
+	//separate archive function from downloading and just append to a a list to dl from later
 	archive(s, lastChatID, channelID)
-	return lastChatID
+	return nil, urlDl
 }
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if strings.HasPrefix(m.Content, "!start") {
-		medium = 0
+		var filename []string
 		s.ChannelMessageSend(m.ChannelID, "Hol' up")
 		args := strings.SplitAfter(m.Content, " ")
 		//last chat ID 176595202172125185 images in the 172,000's
 		//searches thru channel that the command was sent in
-		err := archive(s, args[1], m.ChannelID)
-		if err == "" {
+		err, _ := archive(s, args[1], m.ChannelID)
+		if err != nil {
 			log.Println(err)
-			log.Println("Error reading chat history")
 		}
+		s.ChannelMessageSend(m.ChannelID, "Done! check directory location")
+
+		file, err := os.Open(saveLocation)
+		if err != nil {
+			log.Println(err)
+		}
+		defer file.Close()
+
+		fileList, _ := file.Readdir(0)
+
+		for _, files := range fileList {
+			filename = append(filename, "images/"+files.Name())
+		}
+		//log.Println(filename)
+
+		err = bigZip("photos.zip", filename)
+		if err != nil {
+			log.Println(err)
+		}
+		s.ChannelMessageSend(m.ChannelID, "Zip saved!")
 	}
 }
 func main() {
@@ -157,4 +180,55 @@ func main() {
 	<-sc
 	defer dg.Close()
 	//messageCreate()
+}
+
+func bigZip(filename string, files []string) error {
+	log.Println(filename)
+	newZip, err := os.Create(saveLocation + filename)
+
+	if err != nil {
+		return err
+	}
+
+	defer newZip.Close()
+
+	zipWriter := zip.NewWriter(newZip)
+	defer zipWriter.Close()
+
+	for _, file := range files {
+		if !strings.HasSuffix(file, ".zip") {
+			if err = AddFileToZip(zipWriter, file); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func AddFileToZip(zipWriter *zip.Writer, filename string) error {
+
+	fileToZip, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+	header.Name = filename
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, fileToZip)
+	return err
 }
